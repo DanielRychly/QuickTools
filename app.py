0b6b5e574a94bb03eacc2f9939b62b54f576cf5d
xrychly <xrychly@stuba.sk>
2023-10-14 21:34:12 +0200

    added light/dark theme toggle

--- app.py
+++ app.py
@@ -1,35 +1,43 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, session
 import fitz  # PyMuPDF
 from datetime import datetime
 
 app = Flask(__name__)
+app.secret_key = 'change_me_to_very_secret'
 
 current_year = datetime.now().year
 
-@app.route('/')
+@app.route('/',methods=['GET', 'POST'])
 def home():
-    
+
+    check_theme()
     return render_template("index.html", current_year=current_year)
 
-@app.route('/name_extractor')
+@app.route('/name_extractor',methods=['GET', 'POST'])
 def name_extractor():
+    check_theme()
     return render_template('name_extractor.html', current_year=current_year)
 
-@app.route('/about')
+@app.route('/about',methods=['GET', 'POST'])
 def about():
+    check_theme()
     return render_template('about.html', current_year=current_year)
 
-@app.route('/contact')
+@app.route('/contact',methods=['GET', 'POST'])
 def contact():
+    check_theme()
     return render_template('contact.html', current_year=current_year)
 
-@app.route('/annotation_extractor')
+
+@app.route('/annotation_extractor',methods=['GET', 'POST'])
 def annotation_extractor():
+    check_theme()
     return render_template('annotation_extractor.html', current_year=current_year)
 
 @app.route('/process_pdfs', methods=['GET', 'POST'])
 def process_pdfs():
 
+    check_theme()
     uploaded_file = request.files['pdf_file']  # Get a single uploaded PDF file
     parsed_text = ''
 
@@ -48,6 +56,14 @@ def process_pdfs():
     # For example, you can send the parsed_text to a function for further processing
 
     return render_template('process_pdfs.html', parsed_text=parsed_text, current_year=current_year)
+
+def check_theme():
+    if 'theme' not in session:
+        session['theme'] = 'light'
+    if request.method == 'POST':
+        session['theme'] = 'dark' if session['theme'] == 'light' else 'light'
+        return redirect(url_for('home'))
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
