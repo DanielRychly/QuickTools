b88c768149fd1c9d265aae5f95ba51e5ed3541aa
xrychly <xrychly@stuba.sk>
2023-10-14 09:09:47 +0200

    added GH link, current year

--- app.py
+++ app.py
@@ -1,23 +1,27 @@
 from flask import Flask, render_template, request
 import fitz  # PyMuPDF
+from datetime import datetime
 
 app = Flask(__name__)
 
+current_year = datetime.now().year
+
 @app.route('/')
 def home():
-    return render_template('index.html')
+    
+    return render_template("index.html", current_year=current_year)
 
 @app.route('/name_extractor')
 def name_extractor():
-    return render_template('name_extractor.html')
+    return render_template('name_extractor.html', current_year=current_year)
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    return render_template('about.html', current_year=current_year)
 
 @app.route('/annotation_extractor')
 def annotation_extractor():
-    return render_template('annotation_extractor.html')
+    return render_template('annotation_extractor.html', current_year=current_year)
 
 @app.route('/process_pdfs', methods=['GET', 'POST'])
 def process_pdfs():
@@ -39,7 +43,7 @@ def process_pdfs():
     # Now, you can initiate your parsing logic with the parsed_text
     # For example, you can send the parsed_text to a function for further processing
 
-    return render_template('process_pdfs.html', parsed_text=parsed_text)
+    return render_template('process_pdfs.html', parsed_text=parsed_text, current_year=current_year)
 
 if __name__ == '__main__':
     app.run(debug=True)
